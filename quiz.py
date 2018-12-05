48f6e221f95f3737f4f918cedf3d27e880939932
Lionelkris <hithyshi11@yahoo.co.in>
2018-12-05 17:50:15 +0530

    deleted a unnecessarry print statement

--- quiz.py
+++ quiz.py
@@ -6,7 +6,6 @@ key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
 message = b'gAAAAABcBo5-ZlZyY-TEiJjxlEBpnMUlPy_qeAwQDjkZGJqbmXBL9SzR9qc9WX-wyQ8_ULJfemRgJtoS5lmvX-DknVV34JV3AoKEJTdKhombm2sCS8547rPyRNQ2ImbO-SWCpi-tkIpjZC9qqI3ewynoRkeFVUN6Or97SxYbj9NeufbGk-j73tvPwSWibX6EFGFKDDD4-vUc'
 
 def main():
-    print("""""")
     f = Fernet(key)
     print(f.decrypt(message))
 
